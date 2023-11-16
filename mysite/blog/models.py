78773e433f7c67cc315e758bb5d784e1339dac59
DevmanYur <devmanyur@gmail.com>
2023-11-16 21:14:10 +0300

    User один ко многим поставм

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     class Status(models.TextChoices):
@@ -12,6 +13,11 @@ class Post(models.Model):
     # знаки подчеркивания или дефисы.
     # Было Django Reinhardt: A legend of Jazz
     # Слаг django-reinhardt-legend-jazz
+
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
+
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     # По умолчанию значения поля задаются
