725bea134068fb8d728062c0b3c7780be65190a1
DevmanYur <devmanyur@gmail.com>
2023-11-18 11:02:27 +0300

    Создание представлений списка постов

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+def post_list(request):
+    posts = Post.published.all()
+    return render(request,'blog/post/list.html',{'posts': posts})
 
-# Create your views here.
+
+from django.http import Http404
+def post_detail(request, id):
+    # было
+    # try:
+    #     post = Post.published.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404("No Post found.")
+
+    #стало
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+
+    return render(request,'blog/post/detail.html',{'post': post})
